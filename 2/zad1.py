022496e884fb72012fa7f4adc0847bf35e36b342
Kubabob <j.m.m.bozek@gmail.com>
2024-11-24 21:34:00 +0100

    zrobione 2. sprawozdanie

--- 2/zad1.py
+++ 2/zad1.py
@@ -2,16 +2,23 @@
 # coding: utf-8
 
 # %% In[1]:
+import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
+import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold, train_test_split
+
+sns.set_style("darkgrid")
 
 
 # %% In[2]:
 def RMSE(y_hat: pl.Series, y_obs: pl.DataFrame) -> float:
     return (((y_hat - y_obs.to_series()) ** 2).sum() / y_obs.shape[0]) ** 0.5
+
+def R2(y_pred: pl.Series, y_obs: pl.DataFrame):
+    return 1 - ((y_pred - y_obs.to_series())**2).sum() / ((y_obs.to_series() - y_obs.to_series().mean())**2).sum()
 
 
 # %% In[3]:
@@ -94,19 +101,8 @@ for training_set, validation_set in KFold_model.split(X_training, Y_training):
 descriptors
 
 
-# %% In[71]:
-LinearRegression().fit(
-    X=X_training.with_row_index()
-    .filter(~pl.col("index").is_in(validation_set))
-    .drop(pl.col("index")),
-    y=Y_training.with_row_index()
-    .filter(~pl.col("index").is_in(validation_set))
-    .drop(pl.col("index")),
-)
-
-
 # %% In[88]:
-def RMSE_cv() -> pl.Float64:
+def RMSE_cv(validation_sets, X_training, Y_training) -> pl.Float64:
     residues = pl.Series(dtype=pl.Float64)
 
     for idx, validation_set in enumerate(validation_sets):
@@ -140,36 +136,85 @@ def RMSE_cv() -> pl.Float64:
 
     return np.sqrt((residues**2).sum())
 
-
 # %%
-RMSE_cv()
+def RMSE_c(X_training, Y_training) -> pl.Float64 | float:
 
+    PCR_model = LinearRegression().fit(X_training, Y_training)
 
-# %%
-def RMSE_c():
-    x
-
+    return RMSE(
+        PCR_model.predict(X_training).flatten(),
+        Y_training
+    )
 
 # %%
 def principal_component_plot(n: int):
-    pca_model = PCA(n_components=n)
+    CV_results = []
+    cal_results = []
+    for order in range(1, n+1):
+        pca_model = PCA(n_components=order)
 
-    PC = pl.DataFrame(
-        pca_model.fit_transform(descriptors),
-        schema=[f"PC{i+1}" for i in range(pca_model.n_components_)],
-    )
+        PC = pl.DataFrame(
+            pca_model.fit_transform(descriptors),
+            schema=[f"PC{i+1}" for i in range(pca_model.n_components_)],
+        )
 
-    KFold_model = KFold(n_splits=10, shuffle=True, random_state=0)
+        KFold_model = KFold(n_splits=10, shuffle=True, random_state=0)
 
-    X_training, _, Y_training, _ = train_test_split(
-        PC, Y_obs, test_size=0.33, random_state=42
-    )
+        X_training, _, Y_training, _ = train_test_split(
+            PC, Y_obs, test_size=0.33, random_state=42
+        )
 
-    validation_sets = [
-        validation_set
-        for (_, validation_set) in KFold_model.split(X_training, Y_training)
-    ]
+        validation_sets = [
+            validation_set
+            for (_, validation_set) in KFold_model.split(X_training, Y_training)
+        ]
 
-    _RMSE_cv = RMSE_cv()
+        _RMSE_cv = RMSE_cv(validation_sets, X_training, Y_training)
+        CV_results.append(_RMSE_cv)
 
-    RMSE_c
+        _RMSE_c = RMSE_c(X_training, Y_training)
+        cal_results.append(_RMSE_c)
+
+        print(f'Order: {order}\tCV: {_RMSE_cv}\tC:{_RMSE_c}')
+
+    plt.plot(range(1,n+1), CV_results, label='RMSE_cv')
+    plt.plot(range(1,n+1), cal_results, label='RMSE_c')
+    plt.xticks(np.arange(1,5,1))
+    plt.legend()
+    plt.show()
+
+# %%
+principal_component_plot(4)
+
+# %%
+pca_model = PCA(n_components=2)
+
+PC = pl.DataFrame(
+    pca_model.fit_transform(descriptors),
+    schema=[f"PC{i+1}" for i in range(pca_model.n_components_)],
+)
+
+X_training, X_validation, Y_training, Y_validation = train_test_split(
+    PC, Y_obs, test_size=0.33, random_state=42
+)
+
+significant_model = LinearRegression().fit(X_training, Y_training)
+
+# %%
+R2(
+    significant_model.predict(X_training).flatten(),
+    Y_training
+)
+
+# %%
+RMSE_c(
+    X_training,
+    Y_training
+)
+
+
+# %%
+RMSE(
+    significant_model.predict(X_validation).flatten(),
+    Y_validation
+)
